8061490d6941fafc59e1f52a0c5ccfbeaf80e3d7
Mark Mutugi <mutugimg@gmail.com>
2023-10-15 10:07:04 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print (i)
+    i -= 1
+    print ("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squares = [intgr **2 for intgr in int_list]
+    print(squares)
+    return squares
+
+# square_integers([1,2,3,4,5])
+# square_integers([-1,-2,-3,-4,-5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+    while num <= 100:
+        if num % 3 == 0 and num % 5 == 0:
+            print ("FizzBuzz")
+        elif num % 3 == 0:
+            print ("Fizz")
+        elif num % 5 == 0:
+            print ("Buzz")
+        else:
+            print (num)
+        num +=1
+    
+fizzbuzz()
+
